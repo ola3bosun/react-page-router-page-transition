cb60fdd4b81b551ca7ac068b6e49314e93641c91
tubosun <olatubosundiyaolu@gmail.com>
2025-03-23 03:03:31 +0100

    update navbar link text

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
         <div className='nav-item'>
             <Link className='nav-link' to="/about">
-                click me
+                click me first
             </Link>    
         </div>
         <div className='nav-item'>
